90c23cfc4ec6e4e5f3b733924ece1b0c59f76a6f
Adilmojangos8246 <67742933+Adilmojangos8246@users.noreply.github.com>
2021-01-06 21:56:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+  background(46,  139, 87)
+  drawSprites();
+  //add styles here
+
+}
+
+
+
